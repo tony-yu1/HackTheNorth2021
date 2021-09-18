e820786057a779b93b56d788734153a5ee79fe37
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2021-09-18 01:09:37 -0400

    Update navigation_bar.js

--- my-project/components/navigation_bar.js
+++ my-project/components/navigation_bar.js
@@ -1,9 +1,30 @@
-function navigation_bar() {
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+
+function Navigation_bar() {
     return (
-        <div>
-            
+      
+        
+        <div className="flex items-center"> 
+
+            <h1>hi</h1>
+            <Button 
+                            color="black"
+                            buttonType="outline"
+                            rounded={true}
+                            iconOnly={true}
+                            ripple="dark"
+                            className="md:inline-flex h-20 w-20 border-0"           
+            >
+                <Icon name="menu" size="3xl" />
+            </Button>
+
+            <Icon name="description" size="5xl" color="blue" />
+
+
+
         </div>
     )
 }
 
-export default navigation_bar
+export default Navigation_bar
