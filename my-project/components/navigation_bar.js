2ca3ab19f48365bf772a365ad9f8b9a0f51bc87d
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2021-09-18 11:15:15 -0400

    Update navigation_bar.js
    Finished Navigation bar

--- my-project/components/navigation_bar.js
+++ my-project/components/navigation_bar.js
@@ -24,10 +24,28 @@ function Navigation_bar() {
 
             <h1 className="ml-2 text-black-700 trext-2xl">appname</h1>
 
-            <div className="flex flex-grow ">
-                <icon name="search" size="3xl" color="black" />
-                <input type="text" placeholder="Search" />
+            <div className="ml-5 md:ml-20 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md">
+                <Icon name="search" size="3xl" color="black" />
+                <input type="text" placeholder="Search" className="flex-grow px-5 text-base bg-transparent outline-none"/>
             </div>
+
+            <Button 
+                            color="black"
+                            buttonType="outline"
+                            rounded={true}
+                            iconOnly={true}
+                            ripple="dark"
+                            className="ml-5 md:ml-20 h-20 w-20 border-0"     
+            >
+                <Icon name="apps" size="3xl" color="black"/>
+            </Button>
+
+            <img
+                loading="lazy"
+                className="cursor-pointer h-12 w-12 rounded-full ml-2"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Dwayne_Johnson_2%2C_2013.jpg/800px-Dwayne_Johnson_2%2C_2013.jpg"
+                alt=""
+            ></img>
         </div>
     )
 }
