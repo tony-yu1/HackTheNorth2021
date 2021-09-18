bd19d50cafdd8c9eccabef7df1ba3efb1c28e2c6
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2021-09-18 11:31:19 -0400

    Update index.js
    New project bar

--- my-project/pages/index.js
+++ my-project/pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import Navigation_bar from '../components/navigation_bar.js';
+import Icon from "@material-tailwind/react/Icon";
+import Button from "@material-tailwind/react/Button";
 
 export default function Home() {
   return (
@@ -11,10 +13,27 @@ export default function Home() {
 
       <Navigation_bar />
 
-      <section>
+      <section className='bg-[#f7f7f7] pb-10 px-10'>
         <div className="max-w-3xl mx-auto">
+          <div className="flex items-center justify-between py-6">
+            <h2 className="text-lg">Start a new file</h2>
           
+            <Button 
+                            color="black"
+                            buttonType="outline"
+                            rounded={true}
+                            iconOnly={true}
+                            ripple="dark"
+                            className="border-0"           
+            >
+                <Icon name="more_vert" size="3xl" />
+            </Button>
+          
+          </div>
 
+          <div>
+
+          </div>
         </div>
       </section>
 
