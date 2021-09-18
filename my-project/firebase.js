775c47a53d6e6adc47e376e51be52c8d1a2f84ea
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2021-09-18 14:30:09 -0400

    Create firebase.js
    Firebase

---
+++ my-project/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZTaZgv9mbrCO17voxM09fZ48jNr7D_9A",
+    authDomain: "htn2021-b44bb.firebaseapp.com",
+    projectId: "htn2021-b44bb",
+    storageBucket: "htn2021-b44bb.appspot.com",
+    messagingSenderId: "569219933249",
+    appId: "1:569219933249:web:2c10f31360c4c33effd2c0"
+  };
