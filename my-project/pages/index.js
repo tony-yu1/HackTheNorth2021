63a99aca03e4f7466df51ba1f42553b4f17b7e32
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2021-09-18 12:59:05 -0400

    Update index.js

--- my-project/pages/index.js
+++ my-project/pages/index.js
@@ -36,12 +36,23 @@ export default function Home() {
             <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-black">
             <Image
               src='https://links.papareact.com/pju' layout="fill" />
-
+  
             </div>
 
-            <p>Blank</p>
+            <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">Blank</p>
           </div>
 
+        </div>
+      </section>
+
+      <section className="bg-white px-10 md:px=2 text-sm">
+        <div className="max-w-3xl mx-auto py-8">
+          <div className="flex items-center justify-between pb-5">
+            <h2 className="font-medium flex-grow">My Projects</h2>
+            <p className="mr-6">Date Created</p>
+            <Icon name="folder" size="3xl" />
+
+          </div>
         </div>
       </section>
 
