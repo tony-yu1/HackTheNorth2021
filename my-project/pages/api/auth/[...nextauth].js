77d6350d06685f6b46733457b7bf6348689cfbae
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2021-09-18 16:48:45 -0400

    Revert "Revert "Update [...nextauth].js""
    This reverts commit 850319ea81683cfff124ef9f776db6abfab98bdd.

--- my-project/pages/api/auth/[...nextauth].js
+++ my-project/pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import Providers from "next-auth/providers"
+import { FirebaseAdapter } from "@next-auth/firebase-adapter";
+import { db } from "../../../firebase";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,5 +12,7 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-  database:process.env.DATABASE_URL,
+
+  adapter: FirebaseAdapter(db),
+
 })
